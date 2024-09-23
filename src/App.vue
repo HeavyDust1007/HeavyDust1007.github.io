bc530c6f3b0c5b1f2578b7670ff88f22affc7dfe
HeavyDust1007 <162321624+HeavyDust1007@users.noreply.github.com>
2024-09-23 16:57:09 +0800

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <Layout>
+    <BaseTurbine>
+      <template #left>
+        <component
+          :is="Witgets[item]"
+          v-for="item in chunkModules.left"
+          :key="item"
+        />
+      </template>
+      <template #right>
+        <component
+          :is="Witgets[item]"
+          v-for="item in chunkModules.right"
+          :key="item"
+        />
+      </template>
+      <template #control>
+        <Witgets.ControlPanel></Witgets.ControlPanel>
+        <Witgets.ControlTurbine></Witgets.ControlTurbine>
+      </template>
+    </BaseTurbine>
+  </Layout>
+</template>
+<script setup lang="ts">
+import { storeToRefs } from 'pinia'
+import Layout from './layout/index.vue'
+import { useLayoutStore } from './stores/modules/layout'
+
+import BaseTurbine from './components/BaseTurbine/index.vue'
+import Witgets from './widgets'
+
+const { chunkModules } = storeToRefs(useLayoutStore())
+</script>
